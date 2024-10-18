79a2cf79523936f06d8bebe498ecefb1487c11e4
Piotr Mazurek <piotr635@gmail.com>
2024-10-18 20:55:24 +0200

    quciksort with function pointer added

--- chapter-5/day_of_year.c
+++ chapter-5/day_of_year.c
@@ -2,6 +2,11 @@
 #include <time.h>
 #include "date.h"
 
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
 int day_of_year(int year, int month, int day){
     int i, leap;
 
